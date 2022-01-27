2b0d418d85f3185e9700cfbbe36dbdd4a2d54a2b
BalcouEwennETPA <ewennbalcou10@gmail.com>
2022-01-27 09:26:28 +0100

    commentaires

--- Score_Karaoke_V1.py
+++ Score_Karaoke_V1.py
@@ -1,12 +1,12 @@
 from unicodedata import name
 
 
-class Player:
-    def __init__(self,name) : 
+class Player:   #declaration de la classe
+    def __init__(self,name) : #avoir un pseudo
         self.nom = name
-    def perdPV (self,nombre) :
+    def score (self,nombre) : #score de la personne
         self.score = nombre
-    def prendreChanson (self,Chanson0,Chanson1,Chanson2,Chanson3,Chanson4):
+    def prendreChanson (self,Chanson0,Chanson1,Chanson2,Chanson3,Chanson4): #prendre une chanson
         self.__Chanson0=Chanson0
         self.__Chanson1=Chanson1
         self.__Chanson2=Chanson2
--- Score_Karaoke_V2.py
+++ Score_Karaoke_V2.py
@@ -1,32 +1,35 @@
 class Player:
-    def __init__(self,name) : 
+    def __init__(self,name) : #avoir un nom
         self.nom = name
     
-    def perdPV (self,nombre) :
+    def perdPV (self,nombre) : #score de la personne 
         self.score = nombre
     
-    def prendreChanson (self,Chanson0,Chanson1,Chanson2,Chanson3,Chanson4):
+    def prendreChanson (self,Chanson0,Chanson1,Chanson2,Chanson3,Chanson4): #prendre une musique 
         self.__Chanson0=Chanson0
         self.__Chanson1=Chanson1
         self.__Chanson2=Chanson2
         self.__Chanson3=Chanson3
         self.__Chanson4=Chanson4
     
-    def BestScoreTotal(self,nombre):
+    def BestScoreTotal(self,nombre): #avoir son meilleur score des musiques au total
         self.BestScoreTotal = nombre
     
-    def BestScoreChansons(self,nombre):
+    def BestScoreChansons(self,nombre): #avoir son meilleur score par musique
         self.BestScoreChansons = nombre
     
-    def BestScoreMoyenne(self,nombre):
+    def BestScoreMoyenne(self,nombre): #avoir son score moyen des musiques au total
         self.BestScoreMoyenne = nombre
 
 class Karaoke:
-    def __init__(self,name) :
+    def __init__(self,name) : #avoir le nom de la musique
         self.nomChanson = name
-    def NbrPlayer (self,nombre) :
+
+    def NbrPlayer (self,nombre) : #avoir le nombre de joueurs
         self.NbrPlayer = nombre
-    def BestScore (self,nombre1) :
+    
+    def BestScore (self,nombre1) : #avoir le meilleur score
         self.BestScore = nombre1
-    def NbrChanson(self,nombre2) :
+    
+    def NbrChanson(self,nombre2) : #avoir le numéro de la musique utilisé
         self.NbrChanson = nombre2
