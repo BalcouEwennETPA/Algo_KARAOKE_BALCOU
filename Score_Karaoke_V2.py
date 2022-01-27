2e3ff34602612b8646554d5559a29a4635cb10da
BalcouEwennETPA <ewennbalcou10@gmail.com>
2022-01-27 09:29:10 +0100

    Update Score_Karaoke_V2.py

--- Score_Karaoke_V2.py
+++ Score_Karaoke_V2.py
@@ -6,11 +6,11 @@ class Player:
         self.score = nombre
     
     def prendreChanson (self,Chanson0,Chanson1,Chanson2,Chanson3,Chanson4): #prendre une musique 
-        self.__Chanson0=Chanson0
-        self.__Chanson1=Chanson1
-        self.__Chanson2=Chanson2
-        self.__Chanson3=Chanson3
-        self.__Chanson4=Chanson4
+        self.__Chanson0 = Chanson0
+        self.__Chanson1 = Chanson1
+        self.__Chanson2 = Chanson2
+        self.__Chanson3 = Chanson3
+        self.__Chanson4 = Chanson4
     
     def BestScoreTotal(self,nombre): #avoir son meilleur score des musiques au total
         self.BestScoreTotal = nombre
